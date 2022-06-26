d84b4dc24c24ed2f32ea127830b07fab236332ce
IgorBabikov <igorbabikov91@gmail.com>
2022-06-26 08:20:54 +0300

    added layout for Home, used Outlet react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import {Routes, Route } from "react-router-dom";
-import Header from "../src/components/Header";
 import Home from "./pages/Home";
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import PizzaInfo from "./components/pizzaInfo/PizzaInfo";
+import MaingLayout from "./layout/MaingLayout";
 
 
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/pizza/:id" element={<PizzaInfo/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="*" element={<NotFound/>}/>
-        </Routes>
-     </div>
+    <Routes>
+      <Route path="/" element={<MaingLayout/>}>
+        <Route path="" element={<Home/>}/>
+        <Route path="pizza/:id" element={<PizzaInfo/>}/>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Route>
+    </Routes>
   );
 }
 
