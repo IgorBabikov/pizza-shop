e87b93711489c61125ea78238fbedc2830323501
IgorBabikov <igorbabikov91@gmail.com>
2022-07-02 00:00:02 +0300

    fixed pizzaSlice

--- src/redux/slices/pizzasSlice.ts
+++ src/redux/slices/pizzasSlice.ts
@@ -1,8 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { RootState } from '../store';
-const key = process.env.REACT_APP_KEY;
-
 
 type Pizza = {
   id: string;
@@ -41,7 +39,7 @@ export const fetchPizzas = createAsyncThunk<Pizza[], fetchPizzasArgs>('pizzas/fe
     const { currentPage, category, sorting, order } = params;
 
     const { data } = await axios.get<Pizza[]>(
-      `https://${key}.mockapi.io/pizzas-list/?limit=4&page=${currentPage}${category}&sortBy=${sorting}&order=${order}`,
+      `https://62a54ce7430ba53411c504a9.mockapi.io/pizzas-list/?limit=4&page=${currentPage}${category}&sortBy=${sorting}&order=${order}`,
     );
     return data;
   },
