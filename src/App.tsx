76b250d149187cbb7d938994891d8c1a19f319fb
Igor <igorbabikov91@gmail.com>
2022-07-07 14:35:34 +0300

    added lazy loading for components pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import {Routes, Route } from "react-router-dom";
+import {lazy, Suspense} from 'react'
 import Home from "./pages/Home";
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import PizzaInfo from "./components/pizzaInfo/PizzaInfo";
 import MaingLayout from "./layout/MaingLayout";
+import Spinner from "./components/spinner/Spinner";
 
-
+const PizzaInfo = lazy(() => import(/* webpackChunkName: "PizzaInfo" */ '../src/components/pizzaInfo/PizzaInfo'))
+const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ '../src/pages/Cart'))
+const NotFound = lazy(() => import(/* webpackChunkName: "NotFound" */ '../src/pages/NotFound'))
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MaingLayout/>}>
         <Route path="" element={<Home/>}/>
-        <Route path="pizza/:id" element={<PizzaInfo/>}/>
-        <Route path="cart" element={<Cart/>}/>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="pizza/:id" element={<Suspense><PizzaInfo/></Suspense>}/>
+        <Route path="cart" element={<Suspense fallback={<Spinner/>}><Cart/></Suspense>}/>
+        <Route path="*" element={<Suspense><NotFound/></Suspense>}/>
       </Route>
     </Routes>
   );
